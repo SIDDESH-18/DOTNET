963d035b7aebbd10c89d7715631b630bf7d896db
SIDDESH M <23ecc27@karpagamtech.ac.in>
2025-12-08 12:22:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SnakePattern
+{
+    class program
+    {
+        static void Main()
+        {
+            int[,] matA = new int[4, 4];
+            Console.WriteLine("Enter the elements of the 4x4 matrix row-wise:");
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                   matA[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("The matrix in snake pattern is:");
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Console.Write(matA[i, j] + " ");
+                    }
+                }
+                else
+                {
+                    for (int j = 3; j >= 0; j--)
+                    {
+                        Console.Write(matA[i, j] + " ");
+                    }
+
+                }
+
+            }
+        }
+    }
+}
